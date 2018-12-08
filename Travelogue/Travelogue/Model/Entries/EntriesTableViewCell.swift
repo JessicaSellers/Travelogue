a75b44bf7020fa64f7def89af8e84da539b89720
Jessica Sellers <jessica@jessicas-macbook-pro.local>
2018-12-07 22:13:44 -0600

    added to entries

---
+++ Travelogue/Travelogue/Model/Entries/EntriesTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  EntriesTableViewCell.swift
+//  Travelogue
+//
+//  Created by Jessica Elizabeth Sellers on 12/7/18.
+//  Copyright © 2018 Jessica Sellers. All rights reserved.
+//
+
+import UIKit
+
+class EntriesTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Travelogue/Travelogue/Model/Entries/EntriesViewController.swift
+++ Travelogue/Travelogue/Model/Entries/EntriesViewController.swift
@@ -8,29 +8,42 @@
 
 import UIKit
 
-class EntriesViewController: UIViewController {
+class EntriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
 
+    @IBOutlet weak var entryTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        entryTableView.dataSource = self
+        entryTableView.delegate = self
+        
+        EntriesFunctions.readEntry(completion: { [weak self] in
+            self?.entryTableView.reloadData()
+        })
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.entriesModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = entryTableView.dequeueReusableCell(withIdentifier: "entryCell") as! EntriesTableViewCell
+        
+        //cell.setup(entryModel: Data.entriesModels[indexPath.row])
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
